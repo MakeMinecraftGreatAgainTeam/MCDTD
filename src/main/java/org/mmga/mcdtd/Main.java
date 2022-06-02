465c391c455cfd912647cd6573176514cb3d2e43
Wzp-2008 <18758003180@163>
2022-06-02 23:00:52 +0800

    feat: Initialize the environment

---
+++ src/main/java/org/mmga/mcdtd/Main.java
@@ -0,0 +1,20 @@
+package org.mmga.mcdtd;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author 33572
+ */
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
