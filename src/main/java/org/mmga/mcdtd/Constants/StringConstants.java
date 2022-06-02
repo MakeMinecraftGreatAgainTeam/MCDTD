290ef942d94dfb7b85e70710d5b9591c1d908bcc
Wzp-2008 <18758003180@163>
2022-06-03 00:24:53 +0800

    feat: adding druid config in Main.java

---
+++ src/main/java/org/mmga/mcdtd/Constants/StringConstants.java
@@ -0,0 +1,10 @@
+package org.mmga.mcdtd.Constants;
+
+/**
+ * @author wzp
+ * @version 1.0.0
+ */
+public class StringConstants {
+    public static final String MYSQL = "mysql";
+    public static final String SQLITE = "sqlite";
+}
